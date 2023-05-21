af5c05e8bc9d23181042f6784497d917953d0c58
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-21 15:22:48 +0200

    Create game logic script

---
+++ Assets/_Scripts/Game/GameLogic.cs
@@ -0,0 +1,11 @@
+using Structures;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class GameLogic : MonoBehaviour
+{
+}
