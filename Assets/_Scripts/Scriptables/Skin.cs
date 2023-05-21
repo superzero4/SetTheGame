a9e2638c42fad88a4deb5cdb78e83abb4c0bc7d1
superzero4 <78915984+superzero4@users.noreply.github.com>
2023-05-21 14:37:11 +0200

    Added displayers so user know what the list members should match
    It doesn't change much in the end we could have a rounded sprite on the square shape, logic is agnostic, but can bring more sense to the developper

--- Assets/_Scripts/Scriptables/Skin.cs
+++ Assets/_Scripts/Scriptables/Skin.cs
@@ -12,9 +12,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New skin", menuName = "Scriptables/Skin")]
 public class Skin : ScriptableObject
 {
+    [InfoBox("@" + nameof(EnumList) + "<" + nameof(Count) + ">()")]
     [SerializeField, ValidateInput("@" + nameof(ListMatchesSize) + "<" + nameof(Count) + ">(" + nameof(_count) + ")", "@" + nameof(ErrorMessage) + "<" + nameof(Count) + ">(" + nameof(_count) + ")")] private List<int> _count;
+    [InfoBox("@" + nameof(EnumList) + "<" + nameof(Colors) + ">()")]
     [SerializeField, ValidateInput("@" + nameof(ListMatchesSize) + "<" + nameof(Colors) + ">(" + nameof(_colors) + ")", "@" + nameof(ErrorMessage) + "<" + nameof(Colors) + ">(" + nameof(_colors) + ")")] private List<Color> _colors;
+    [InfoBox("@" + nameof(EnumList) + "<" + nameof(Fill) + ">()")]
     [SerializeField, ValidateInput("@" + nameof(ListMatchesSize) + "<" + nameof(Fill) + ">(" + nameof(_materials) + ")", "@" + nameof(ErrorMessage) + "<" + nameof(Fill) + ">(" + nameof(_materials) + ")")] private List<Material> _materials;
+    [InfoBox("@" + nameof(EnumList) + "<" + nameof(Shape) + ">()")]
     [SerializeField, ValidateInput("@" + nameof(ListMatchesSize) + "<" + nameof(Shape) + ">(" + nameof(_sprites) + ")", "@" + nameof(ErrorMessage) + "<" + nameof(Shape) + ">(" + nameof(_sprites) + ")")] private List<Sprite> _sprites;
     public Result this[CardData indexer] => (indexer.Count, _colors[indexer.Color], _materials[indexer.Fill], _sprites[indexer.Shape]);
     public IList this[DataType t]
@@ -46,6 +50,7 @@ public class Skin : ScriptableObject
     }
     private bool ListMatchesSize<T>(IList list) where T : Enum => list.Count == EnumHelpers.Count<T>();
     private string ErrorMessage<T>(IList list) where T : Enum => "There is " + list.Count + " instead of " + EnumHelpers.Count<T>() + " values";
+    private string EnumList<T>() where T : Enum => "Member IN ORDER are : " + string.Join(", ", EnumHelpers.Values<T>().Select(e => e.ToString()));
     public static bool ValidateSkin(Skin skin) => skin != null && skin.IsValid();
 #if UNITY_EDITOR
 
